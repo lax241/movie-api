592f944f67827d4ea3bccdec764fa2448c37ff0a
nawro <lax241@gmail.com>
2018-12-04 23:55:14 +0100

    Restore @Data adnotation

--- src/main/java/io/interview/api/Genres.java
+++ src/main/java/io/interview/api/Genres.java
@@ -1,10 +1,11 @@
 package io.interview.api;
 
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.constraints.NotNull;
-
+@Data
 public class Genres {
 
     @NotNull
